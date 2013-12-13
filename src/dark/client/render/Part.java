8fc14c139aa3420a9de6cd42d105254b2a7cfa8f
Robert <rseifert.phone@gmail.com>
2013-12-13 01:26:38 -0500

    more animation init files

---
+++ src/dark/client/render/Part.java
@@ -0,0 +1,15 @@
+package dark.client.render;
+
+import dark.Vector3;
+
+public class Part
+{
+    Vector3 postion;
+    Vector3 rotation;
+
+    public Part(Vector3 postion, Vector3 rotation)
+    {
+        this.postion = postion;
+        this.rotation = rotation;
+    }
+}
