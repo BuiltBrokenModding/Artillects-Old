b5a6f88fe56f41072023538b960c30662f224ea1
Robert <rseifert.phone@gmail.com>
2013-12-13 01:21:02 -0500

    Started on items

---
+++ src/dark/items/ItemDroneSpawner.java
@@ -0,0 +1,13 @@
+package dark.items;
+
+/** Debug tool use to spawn drones in the same way that monster spawn eggs do.
+ * 
+ * @author Dark */
+public class ItemDroneSpawner extends ItemBase
+{
+    public ItemDroneSpawner()
+    {
+        super("DroneSpawner");
+    }
+
+}
