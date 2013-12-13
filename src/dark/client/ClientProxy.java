256c61f07c8a68d45b0b7514b09d38eecdb64751
Robert <rseifert.phone@gmail.com>
2013-12-13 00:31:09 -0500

    Starter classes

---
+++ src/dark/client/ClientProxy.java
@@ -0,0 +1,14 @@
+package dark.client;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
+import dark.CommonProxy;
+
+public class ClientProxy extends CommonProxy
+{
+    public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
