b59f2c336ccc5f3e697cb2fbb35577d2f8c623be
Robert <rseifert.phone@gmail.com>
2013-12-13 01:21:14 -0500

    Created an enum for drone loading

---
+++ src/dark/entity/Drones.java
@@ -0,0 +1,137 @@
+package dark.entity;
+
+import java.awt.Color;
+
+import cpw.mods.fml.common.registry.EntityRegistry;
+import dark.ModDrones;
+
+public enum Drones
+{
+    WORKER(new IDroneBuilder()
+    {
+
+        @Override
+        public void register()
+        {
+            EntityRegistry.registerGlobalEntityID(EntityDroneWorker.class, "DroneWorker", EntityRegistry.findGlobalUniqueEntityId());
+            EntityRegistry.registerModEntity(EntityDroneWorker.class, "DroneWorker", ids++, ModDrones.instance(), 64, 1, true);
+            //EntityRegistry.addSpawn(Entity.class, 3, 1, 10, EnumCreatureType.creature, BiomeGenBase.forest, BiomeGenBase.river);
+        }
+
+        @Override
+        public EntityDrone getNew()
+        {
+            // TODO Auto-generated method stub
+            return null;
+        }
+    }),
+    FABRICATOR(new IDroneBuilder()
+    {
+
+        @Override
+        public void register()
+        {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public EntityDrone getNew()
+        {
+            // TODO Auto-generated method stub
+            return null;
+        }
+    }),
+    CONSTRUCTOR(new IDroneBuilder()
+    {
+
+        @Override
+        public void register()
+        {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public EntityDrone getNew()
+        {
+            // TODO Auto-generated method stub
+            return null;
+        }
+    }),
+    HIVEMIND(new IDroneBuilder()
+    {
+
+        @Override
+        public void register()
+        {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public EntityDrone getNew()
+        {
+            // TODO Auto-generated method stub
+            return null;
+        }
+    }),
+    COMBAT(new IDroneBuilder()
+    {
+
+        @Override
+        public void register()
+        {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public EntityDrone getNew()
+        {
+            // TODO Auto-generated method stub
+            return null;
+        }
+    }),
+    COMBAT_DISC(new IDroneBuilder()
+    {
+
+        @Override
+        public void register()
+        {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public EntityDrone getNew()
+        {
+            // TODO Auto-generated method stub
+            return null;
+        }
+    }),
+    ZOMBIE(new IDroneBuilder()
+    {
+
+        @Override
+        public void register()
+        {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public EntityDrone getNew()
+        {
+            // TODO Auto-generated method stub
+            return null;
+        }
+    });
+    public static int ids = 54;
+    IDroneBuilder builder;
+
+    private Drones(IDroneBuilder builder)
+    {
+        this.builder = builder;
+    }
+}
---
+++ src/dark/entity/IDroneBuilder.java
@@ -0,0 +1,13 @@
+package dark.entity;
+
+/** Used only by the drones enum to make it easier to register new drones
+ * 
+ * @author Dark */
+public interface IDroneBuilder
+{
+    /** Called to register everything that is need for the drone to be loaded into the game */
+    public void register();
+
+    /** Called to create a new instance of the drone */
+    public EntityDrone getNew();
+}
