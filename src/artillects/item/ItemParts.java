75ccb15cf1f300ef433f13f487f320cf04ab1914
Calclavia <admin@calclavia.com>
2013-12-14 12:18:20 +0800

    Added circuit names

--- src/artillects/item/ItemParts.java
+++ src/artillects/item/ItemParts.java
@@ -10,76 +10,74 @@ import artillects.Artillects;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
-/** Parts and materials used in the machines.
+/**
+ * Parts and materials used in the machines.
  * 
- * @author DarkGuardsman */
+ * @author DarkGuardsman
+ */
 public class ItemParts extends ItemBase
 {
-    public ItemParts()
-    {
-        super("DroneParts");
-        this.setHasSubtypes(true);
-    }
+	public ItemParts()
+	{
+		super("droneParts");
+		this.setHasSubtypes(true);
+	}
 
-    @Override
-    public String getUnlocalizedName(ItemStack itemStack)
-    {
-        if (itemStack.getItemDamage() < Part.values().length)
-        {
-            return "item." + Part.values()[itemStack.getItemDamage()].name;
-        }
-        return super.getUnlocalizedName(itemStack);
-    }
+	@Override
+	public String getUnlocalizedName(ItemStack itemStack)
+	{
+		if (itemStack.getItemDamage() < Part.values().length)
+		{
+			return super.getUnlocalizedName(itemStack) + "." + Part.values()[itemStack.getItemDamage()].name;
+		}
+		return super.getUnlocalizedName(itemStack);
+	}
 
-    @Override
-    @SideOnly(Side.CLIENT)
-    public void getSubItems(int par1, CreativeTabs par2CreativeTabs, List par3List)
-    {
-        for (Part part : Part.values())
-        {
-            par3List.add(new ItemStack(this, 1, part.ordinal()));
-        }
-    }
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void getSubItems(int par1, CreativeTabs par2CreativeTabs, List par3List)
+	{
+		for (Part part : Part.values())
+		{
+			par3List.add(new ItemStack(this, 1, part.ordinal()));
+		}
+	}
 
-    @SideOnly(Side.CLIENT)
-    @Override
-    public Icon getIconFromDamage(int meta)
-    {
-        if (meta < Part.values().length)
-        {
-            return Part.values()[meta].icon;
-        }
-        return this.itemIcon;
-    }
+	@SideOnly(Side.CLIENT)
+	@Override
+	public Icon getIconFromDamage(int meta)
+	{
+		if (meta < Part.values().length)
+		{
+			return Part.values()[meta].icon;
+		}
+		return this.itemIcon;
+	}
 
-    @Override
-    @SideOnly(Side.CLIENT)
-    public void registerIcons(IconRegister par1IconRegister)
-    {
-        super.registerIcons(par1IconRegister);
-        for (Part part : Part.values())
-        {
-            part.icon = par1IconRegister.registerIcon(Artillects.PREFIX + "dronepart." + part.name);
-        }
-    }
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IconRegister par1IconRegister)
+	{
+		super.registerIcons(par1IconRegister);
+		for (Part part : Part.values())
+		{
+			part.icon = par1IconRegister.registerIcon(Artillects.PREFIX + "dronepart." + part.name);
+		}
+	}
 
-    public static enum Part
-    {
-        GEARS("gears"),
-        MELTED_CICUITS("meltedCircuits"),
-        T2_MELTED_CICUITS("meltedCircuits2"),
-        T3_MELTED_CICUITS("meltedCircuits3"),
-        CICUITS("circuits"),
-        T2_CICUITS("circuits2"),
-        T3_CICUITS("circuits3");
+	public static enum Part
+	{
+		GEARS("gears"), CICUITS_MELTED_T1("meltedCircuits1"),
+		CIRCUITS_MELTED_T2("meltedCircuits2"), CICUITS_MELTED_T3("meltedCircuits3"),
+		CIRCUITS_T1("circuits1"), CIRCUITS_T2("circuits2"), CIRCUITS_T3("circuits3");
 
-        public final String name;
-        public Icon icon;
+		public final String name;
+		public Icon icon;
 
-        private Part(String name)
-        {
-            this.name = name;
-        }
+		private Part(String name)
+		{
+			this.name = name;
+		}
 
-    }
+	}
 }
