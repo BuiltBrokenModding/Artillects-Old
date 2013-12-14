7bc377ed65295b8d20ec664667eef042321d79fc
Calclavia <admin@calclavia.com>
2013-12-14 14:03:34 +0800

    Fixed shift-click crash

--- src/artillects/container/ContainerWorker.java
+++ src/artillects/container/ContainerWorker.java
@@ -14,12 +14,12 @@ public class ContainerWorker extends ContainerBase
 
 	public ContainerWorker(EntityWorker worker, EntityPlayer player)
 	{
-		super(player.inventory);
+		super(worker.inventory);
 		this.player = player;
 
-		for (int i = 0; i < 3; i++)
+		for (int j = 0; j < 3; j++)
 		{
-			for (int j = 0; j < 3; j++)
+			for (int i = 0; i < 3; i++)
 			{
 				this.addSlotToContainer(new Slot(worker.inventory, i + j * 3, 111 + i * 18, -4 + j * 18));
 			}
