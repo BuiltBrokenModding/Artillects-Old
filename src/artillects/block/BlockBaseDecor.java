cd6ec06196ade96e1d9aa10b1a8e3a328faa769d
Archtikz <atroglyphics@gmail.com>
2013-12-14 22:18:26 +0000

    Added ability to turn on and off Hive Lights

--- src/artillects/block/BlockBaseDecor.java
+++ src/artillects/block/BlockBaseDecor.java
@@ -6,11 +6,36 @@ import net.minecraft.world.World;
 import artillects.Artillects;
 
 public class BlockBaseDecor extends BlockBase {
-
-	short textureMode = 0;
+	
+	private boolean lightPos = true;
 	
 	public BlockBaseDecor(String name) {
 		super(name, Material.rock);
 		setTextureName(Artillects.PREFIX + name);
+	}
+	
+	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int i, float f1, float f2, float f3) {
+		if(player.isSneaking()) {
+			return false;
+		}
+		
+		if(world.isRemote) {
+			if(this == Artillects.blockLight) {
+				if(lightPos == false) {
+					this.setLightValue(1F);
+					lightPos = true;
+					world.markBlockForRenderUpdate(x, y, z);
+					return true;
+				}
+				if(lightPos == true) {
+					this.setLightValue(0F);
+					lightPos = false;
+					world.markBlockForRenderUpdate(x, y, z);
+					return true;
+				}
+				System.out.println("UPDATED!");
+			}
+		}
+		return false;
 	}
 }
--- src/artillects/item/ItemWeapon.java
+++ src/artillects/item/ItemWeapon.java
@@ -1,5 +1,9 @@
 package artillects.item;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.projectile.EntitySnowball;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 import artillects.Artillects;
 
 
@@ -9,5 +13,15 @@ public class ItemWeapon extends ItemBase {
 		super("tommyGun");
 		setTextureName(Artillects.PREFIX + "tommygun");
 	}
-
+	
+	public ItemStack onItemRightClick(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer) {
+		if (!par3EntityPlayer.capabilities.isCreativeMode) {
+			--par1ItemStack.stackSize;
+		}
+		par2World.playSoundAtEntity(par3EntityPlayer, "random.bow", 0.5F, 0.4F / (itemRand.nextFloat() * 0.4F + 0.8F));
+		if (!par2World.isRemote) {
+			par2World.spawnEntityInWorld(new EntitySnowball(par2World, par3EntityPlayer));
+		}
+		return par1ItemStack;
+	}
 }
