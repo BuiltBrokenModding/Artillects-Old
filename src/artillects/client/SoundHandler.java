931f96ffe54ea806697111ef15a678ac9240d461
Calclavia <admin@calclavia.com>
2013-12-15 21:15:03 +0800

    Made sound load

--- src/artillects/client/SoundHandler.java
+++ src/artillects/client/SoundHandler.java
@@ -1,14 +1,17 @@
 package artillects.client;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraftforge.client.event.sound.SoundLoadEvent;
 import net.minecraftforge.event.ForgeSubscribe;
 import artillects.Artillects;
 
+@SideOnly(Side.CLIENT)
 public class SoundHandler
 {
 	public static final SoundHandler INSTANCE = new SoundHandler();
 
-	public static final String[] SOUND_FILES = { "" };
+	public static final String[] SOUND_FILES = { "voice-agressionDetected.ogg", "voice-intruderAlert.ogg", "voice-pleaseDisarmYourself.ogg", "voice-theHiveIsUnderAttack.ogg" };
 
 	@ForgeSubscribe
 	public void loadSoundEvents(SoundLoadEvent event)
