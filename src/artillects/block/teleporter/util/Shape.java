e820f30f6c5e2c2cac51e100778441e2b4a3c09e
Archtikz <atroglyphics@gmail.com>
2013-12-15 09:31:00 +0000

    Added a bunch of textures

--- src/artillects/block/teleporter/util/Shape.java
+++ src/artillects/block/teleporter/util/Shape.java
@@ -3,7 +3,7 @@ package artillects.block.teleporter.util;
 public enum Shape {
 	NOTHING(null, 0),
 	SQUARE("square", 1),
-	TRIANGLE("triangle", 2),
+	PENTAGON("pentagon", 2),
 	CIRCLE("circle", 3),
 	CROSS("cross", 4);
 	
