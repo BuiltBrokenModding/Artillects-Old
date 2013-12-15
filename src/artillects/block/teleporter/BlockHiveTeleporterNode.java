f16857bfa27d0b8d35e1bfad06040b1e8938aa0f
Archtikz <atroglyphics@gmail.com>
2013-12-15 10:25:13 +0000

    A lot of textures

--- src/artillects/block/teleporter/BlockHiveTeleporterNode.java
+++ src/artillects/block/teleporter/BlockHiveTeleporterNode.java
@@ -1,7 +1,9 @@
 package artillects.block.teleporter;
 
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Icon;
 import net.minecraft.world.World;
 import artillects.Artillects;
 import artillects.block.BlockBase;
@@ -9,10 +11,11 @@ import artillects.block.teleporter.tile.TileHiveTNode;
 
 public class BlockHiveTeleporterNode extends BlockBase
 {
+	public Icon iconTop, iconSide, iconBot;
+	
 	public BlockHiveTeleporterNode()
 	{
 		super("teleporterNode", Material.iron);
-		setTextureName(Artillects.PREFIX + "teleporterNode");
 	}
 
 	public boolean hasTileEntity()
@@ -23,5 +26,17 @@ public class BlockHiveTeleporterNode extends BlockBase
 	public TileEntity createTileEntity(World world, int metadata)
 	{
 		return new TileHiveTNode();
+	}
+	
+	public Icon getIcon(int side, int metadata) {
+		if(side == 0) return iconBot;
+		if(side == 1) return iconTop;
+		return iconSide;
+	}
+	
+	public void registerIcons(IconRegister ir) {
+		this.iconTop = ir.registerIcon(Artillects.PREFIX + "teleporterNode_top");
+		this.iconSide = ir.registerIcon(Artillects.PREFIX + "teleporterNode_side");
+		this.iconBot = ir.registerIcon(Artillects.PREFIX + "decorWall1");
 	}
 }
