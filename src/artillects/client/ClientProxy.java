4e6e1ef384f9987dca620c76119833098724a634
Robert <rseifert.phone@gmail.com>
2013-12-16 06:37:30 -0500

    added another combat drone

--- src/artillects/client/ClientProxy.java
+++ src/artillects/client/ClientProxy.java
@@ -10,6 +10,7 @@ import artillects.CommonProxy;
 import artillects.Vector3;
 import artillects.client.gui.GuiArtillect;
 import artillects.client.render.RenderArtillectItems;
+import artillects.client.render.RenderCombatDrone;
 import artillects.client.render.RenderDemolisher;
 import artillects.client.render.RenderFabricator;
 import artillects.client.render.RenderSeeker;
@@ -17,6 +18,7 @@ import artillects.client.render.RenderWorker;
 import artillects.entity.EntityFabricator;
 import artillects.entity.EntityWorker;
 import artillects.entity.IArtillect;
+import artillects.entity.combat.EntityCombatDrone;
 import artillects.entity.combat.EntityDemolisher;
 import artillects.entity.combat.EntitySeeker;
 import cpw.mods.fml.client.FMLClientHandler;
@@ -24,36 +26,37 @@ import cpw.mods.fml.client.registry.RenderingRegistry;
 
 public class ClientProxy extends CommonProxy
 {
-	@Override
-	public void init()
-	{
-		RenderingRegistry.registerEntityRenderingHandler(EntitySeeker.class, new RenderSeeker());
-		RenderingRegistry.registerEntityRenderingHandler(EntityDemolisher.class, new RenderDemolisher());
-		RenderingRegistry.registerEntityRenderingHandler(EntityFabricator.class, new RenderFabricator());
-		RenderingRegistry.registerEntityRenderingHandler(EntityWorker.class, new RenderWorker());
-		MinecraftForgeClient.registerItemRenderer(Artillects.itemArtillectSpawner.itemID, new RenderArtillectItems());
-		MinecraftForge.EVENT_BUS.register(new SoundHandler());
-	}
+    @Override
+    public void init()
+    {
+        RenderingRegistry.registerEntityRenderingHandler(EntitySeeker.class, new RenderSeeker());
+        RenderingRegistry.registerEntityRenderingHandler(EntityDemolisher.class, new RenderDemolisher());
+        RenderingRegistry.registerEntityRenderingHandler(EntityFabricator.class, new RenderFabricator());
+        RenderingRegistry.registerEntityRenderingHandler(EntityWorker.class, new RenderWorker());
+        RenderingRegistry.registerEntityRenderingHandler(EntityCombatDrone.class, new RenderCombatDrone());
+        MinecraftForgeClient.registerItemRenderer(Artillects.itemArtillectSpawner.itemID, new RenderArtillectItems());
+        MinecraftForge.EVENT_BUS.register(new SoundHandler());
+    }
 
-	@Override
-	public Object getClientGuiElement(int id, EntityPlayer player, World world, int x, int y, int z)
-	{
-		if (id == GuiIDs.ARTILLECT_ENTITY.ordinal())
-		{
-			Entity entity = world.getEntityByID(x);
+    @Override
+    public Object getClientGuiElement(int id, EntityPlayer player, World world, int x, int y, int z)
+    {
+        if (id == GuiIDs.ARTILLECT_ENTITY.ordinal())
+        {
+            Entity entity = world.getEntityByID(x);
 
-			if (entity instanceof IArtillect)
-			{
-				return new GuiArtillect((IArtillect) world.getEntityByID(x), player);
-			}
-		}
+            if (entity instanceof IArtillect)
+            {
+                return new GuiArtillect((IArtillect) world.getEntityByID(x), player);
+            }
+        }
 
-		return null;
-	}
+        return null;
+    }
 
-	@Override
-	public void renderLaser(World world, Vector3 start, Vector3 end, float r, float g, float b)
-	{
-		FMLClientHandler.instance().getClient().effectRenderer.addEffect(new FxLaser(world, start, end, r, g, b, 20));
-	}
+    @Override
+    public void renderLaser(World world, Vector3 start, Vector3 end, float r, float g, float b)
+    {
+        FMLClientHandler.instance().getClient().effectRenderer.addEffect(new FxLaser(world, start, end, r, g, b, 20));
+    }
 }
---
+++ src/artillects/entity/combat/EntityCombatDrone.java
@@ -0,0 +1,13 @@
+package artillects.entity.combat;
+
+import net.minecraft.world.World;
+
+public class EntityCombatDrone extends EntityDemolisher
+{
+
+    public EntityCombatDrone(World par1World)
+    {
+        super(par1World);
+    }
+
+}
