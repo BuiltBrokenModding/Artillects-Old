342b4bcb92b05351cafa100ea6e45a4faeffcb3d
Calclavia <admin@calclavia.com>
2013-12-16 17:17:50 +0800

    Made anchor not ticking, inefficient

--- src/artillects/block/teleporter/TileEntityTeleporterAnchor.java
+++ src/artillects/block/teleporter/TileEntityTeleporterAnchor.java
@@ -12,130 +12,116 @@ import artillects.tile.TileEntityAdvanced;
 /** @author Archadia */
 public class TileEntityTeleporterAnchor extends TileEntityAdvanced
 {
-    private long lastFrequencyCheck = 0;
-    private int frequency = 0;
-    private boolean forceXYZ = false;
-    private Vector3 teleportSpot = null;
+	private long lastFrequencyCheck = 0;
+	private int frequency = 0;
+	private boolean forceXYZ = false;
+	private Vector3 teleportSpot = null;
 
-    public void setTeleportLocation(Vector3 location)
-    {
-        if (location != null)
-        {
-            this.forceXYZ = true;
-            this.teleportSpot = location;
-        }
-    }
+	public void setTeleportLocation(Vector3 location)
+	{
+		if (location != null)
+		{
+			this.forceXYZ = true;
+			this.teleportSpot = location;
+		}
+	}
 
-    @Override
-    public void validate()
-    {
-        if (!worldObj.isRemote)
-        {
-            TeleportManager.addAnchor(this);
-        }
-        super.validate();
-    }
+	@Override
+	public void validate()
+	{
+		if (!worldObj.isRemote)
+		{
+			TeleportManager.addAnchor(this);
+		}
 
-    @Override
-    public void invalidate()
-    {
-        if (!worldObj.isRemote)
-        {
-            TeleportManager.remAnchor(this);
-        }
-        super.invalidate();
-    }
+		super.validate();
+	}
 
-    public void refresh()
-    {
-        this.invalidate();
-        this.validate();
-    }
+	@Override
+	public void invalidate()
+	{
+		if (!worldObj.isRemote)
+		{
+			TeleportManager.remAnchor(this);
+		}
 
-    @Override
-    public boolean canUpdate()
-    {
-        return true;
-    }
+		super.invalidate();
+	}
 
-    @Override
-    public void updateEntity()
-    {
-        super.updateEntity();
-        if (worldObj.getWorldTime() % 600 == 0)
-        {
-            refresh();
-        }
-    }
+	@Override
+	public boolean canUpdate()
+	{
+		return false;
+	}
 
-    public void doTeleport(Entity entity)
-    {
-        VectorWorld teleportSpot = null;
-        if (this.forceXYZ && this.teleportSpot != null)
-        {
-            teleportSpot = new VectorWorld(this.worldObj, this.teleportSpot);
-        }
-        else if (this.getFrequency() > 0)
-        {
-            TileEntityTeleporterAnchor teleporter = TeleportManager.getClosestWithFrequency(new VectorWorld(this), this.getFrequency(), this);
-            if (teleporter != null)
-            {
-                teleportSpot = new VectorWorld(teleporter).add(0.5, 2, 0.5);
-            }
-        }
+	public void doTeleport(Entity entity)
+	{
+		VectorWorld teleportSpot = null;
+		if (this.forceXYZ && this.teleportSpot != null)
+		{
+			teleportSpot = new VectorWorld(this.worldObj, this.teleportSpot);
+		}
+		else if (this.getFrequency() > 0)
+		{
+			TileEntityTeleporterAnchor teleporter = TeleportManager.getClosestWithFrequency(new VectorWorld(this), this.getFrequency(), this);
+			if (teleporter != null)
+			{
+				teleportSpot = new VectorWorld(teleporter).add(0.5, 2, 0.5);
+			}
+		}
 
-        if (teleportSpot != null)
-        {
-            TeleportManager.moveEntity(entity, teleportSpot);
-        }
-    }
+		if (teleportSpot != null)
+		{
+			TeleportManager.moveEntity(entity, teleportSpot);
+		}
+	}
 
-    /** @return -1 if the teleporter is unable to teleport. */
-    public int getFrequency()
-    {
-        if (System.currentTimeMillis() - this.lastFrequencyCheck > 10)
-        {
-            this.lastFrequencyCheck = System.currentTimeMillis();
-            this.frequency = 0;
-            for (int i = 2; i < 6; i++)
-            {
-                ForgeDirection direction = ForgeDirection.getOrientation(i);
-                Vector3 position = new Vector3(this.xCoord + direction.offsetX, this.yCoord + direction.offsetY, this.zCoord + direction.offsetZ);
+	/** @return -1 if the teleporter is unable to teleport. */
+	public int getFrequency()
+	{
+		if (System.currentTimeMillis() - this.lastFrequencyCheck > 10)
+		{
+			this.lastFrequencyCheck = System.currentTimeMillis();
+			this.frequency = 0;
+			for (int i = 2; i < 6; i++)
+			{
+				ForgeDirection direction = ForgeDirection.getOrientation(i);
+				Vector3 position = new Vector3(this.xCoord + direction.offsetX, this.yCoord + direction.offsetY, this.zCoord + direction.offsetZ);
 
-                Block block = Block.blocksList[this.worldObj.getBlockId((int) position.x, (int) position.y, (int) position.z)];
+				Block block = Block.blocksList[this.worldObj.getBlockId((int) position.x, (int) position.y, (int) position.z)];
 
-                if (block instanceof BlockGlyph)
-                {
-                    int metadata = this.worldObj.getBlockMetadata((int) position.x, (int) position.y, (int) position.z);
-                    this.frequency += Math.pow(BlockGlyph.MAX_GLYPH, i - 2) * metadata;
-                }
-                else
-                {
-                    return -1;
-                }
-            }
-        }
-        return this.frequency;
-    }
+				if (block instanceof BlockGlyph)
+				{
+					int metadata = this.worldObj.getBlockMetadata((int) position.x, (int) position.y, (int) position.z);
+					this.frequency += Math.pow(BlockGlyph.MAX_GLYPH, i - 2) * metadata;
+				}
+				else
+				{
+					return -1;
+				}
+			}
+		}
+		return this.frequency;
+	}
 
-    @Override
-    public void readFromNBT(NBTTagCompound nbt)
-    {
-        super.readFromNBT(nbt);
-        if (this.teleportSpot != null)
-        {
-            nbt.setCompoundTag("teleportLocation", this.teleportSpot.save(new NBTTagCompound()));
-        }
-    }
+	@Override
+	public void readFromNBT(NBTTagCompound nbt)
+	{
+		super.readFromNBT(nbt);
+		if (this.teleportSpot != null)
+		{
+			nbt.setCompoundTag("teleportLocation", this.teleportSpot.save(new NBTTagCompound()));
+		}
+	}
 
-    @Override
-    public void writeToNBT(NBTTagCompound nbt)
-    {
-        super.writeToNBT(nbt);
-        if (nbt.hasKey("teleportLocation"))
-        {
-            this.teleportSpot = new Vector3(nbt.getCompoundTag("teleportLocation"));
-            this.forceXYZ = true;
-        }
-    }
+	@Override
+	public void writeToNBT(NBTTagCompound nbt)
+	{
+		super.writeToNBT(nbt);
+		if (nbt.hasKey("teleportLocation"))
+		{
+			this.teleportSpot = new Vector3(nbt.getCompoundTag("teleportLocation"));
+			this.forceXYZ = true;
+		}
+	}
 }
