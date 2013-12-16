cc0fa058140919b4d97623ee52bdbfe89af8e07b
Archtikz <atroglyphics@gmail.com>
2013-12-16 08:19:52 +0000

    Entity Changes

--- src/artillects/entity/weapon/EntityPlasma.java
+++ src/artillects/entity/weapon/EntityPlasma.java
@@ -1,24 +1,42 @@
 package artillects.entity.weapon;
 
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.IProjectile;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.projectile.EntityThrowable;
-import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.World;
 
 public class EntityPlasma extends EntityThrowable {
 
+	private int groundLife = 0;
+	boolean impacted = false;
 	private int lifetime = 0;
 	
 	public EntityPlasma(World par1World, EntityLivingBase par2EntityLivingBase) {
 		super(par1World, par2EntityLivingBase);
 	}
+	
+	public void onUpdate() {
+		super.onUpdate();
+		if(impacted) {
+			groundLife++;
+			if(groundLife==30) {
+				worldObj.createExplosion(this, posX, posY, posZ, 5, false);
+				this.setDead();
+				impacted = false;
+			}
+		}
+	}
 
+	public void onCollideWithPlayer(EntityPlayer player) {
+
+	}
+	
 	@Override
 	protected void onImpact(MovingObjectPosition movingobjectposition) {
-		this.worldObj.createExplosion(this, posX, posY, posZ, 1, false);
-		this.setDead();
+		impacted = true;
+		motionX = 0;
+		motionZ = 0;
+		motionY = 0;
 	}
 }
