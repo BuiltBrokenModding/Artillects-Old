23e806fc8a5154c4baef391e2f9db6d53cad3f27
Robert <rseifert.phone@gmail.com>
2013-12-16 21:36:06 -0500

    re-add audio spam check to teleporter base

--- src/artillects/block/teleporter/BlockTeleporterAnchor.java
+++ src/artillects/block/teleporter/BlockTeleporterAnchor.java
@@ -71,10 +71,11 @@ public class BlockTeleporterAnchor extends BlockBase implements ITileEntityProvi
                             player.addChatMessage(Artillects.getLocal("msg.teleporter.frequency") + " " + frequency);
                         }
 
-                        //if (System.currentTimeMillis() - ((TileEntityTeleporterAnchor) tile).lastVoiceActivation > 20 * 600)
-
-                        ((TileEntityTeleporterAnchor) tile).lastVoiceActivation = System.currentTimeMillis();
-                        world.playSoundEffect(x + 0.5, y + 0.5, z + 0.5, Artillects.PREFIX + "voice-introduce-teleporter", 5F, 1F);
+                        if (System.currentTimeMillis() - ((TileEntityTeleporterAnchor) tile).lastVoiceActivation > 20 * 600)
+                        {
+                            ((TileEntityTeleporterAnchor) tile).lastVoiceActivation = System.currentTimeMillis();
+                            world.playSoundEffect(x + 0.5, y + 0.5, z + 0.5, Artillects.PREFIX + "voice-introduce-teleporter", 5F, 1F);
+                        }
                     }
                     else
                     {
