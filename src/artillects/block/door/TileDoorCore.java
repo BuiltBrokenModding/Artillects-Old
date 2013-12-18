8a0b274ea91bd65d2adb5e13f9d9cb5e6f2fd605
Archtikz <atroglyphics@gmail.com>
2013-12-18 08:20:20 +0000

    Setting up

--- src/artillects/block/door/TileDoorCore.java
+++ src/artillects/block/door/TileDoorCore.java
@@ -4,4 +4,11 @@ import net.minecraft.tileentity.TileEntity;
 
 public class TileDoorCore extends TileEntity {
 
+	public boolean isFrameComplete() {
+		return true;
+	}
+	
+	public void toggleFrame() {
+		
+	}
 }
