215ff7e4f1b33ad92680a896924cd170c7482880
DarkGaurdsman <rseifert.phone@gmail.com>
2013-12-29 21:13:25 -0500

    Removed unfinished animation handler

--- src/artillects/client/model/ModelArtillect.java
+++ src/artillects/client/model/ModelArtillect.java
@@ -1,22 +1,8 @@
 package artillects.client.model;
 
 import net.minecraft.client.model.ModelBase;
-import artillects.client.render.Animation;
 
 public abstract class ModelArtillect extends ModelBase
 {
-	int frame = 0;
-	Animation animation;
-
-	public void setFrame(int frame)
-	{
-		this.frame = frame;
-	}
-
-	public void setAnimation(Animation animation)
-	{
-		this.animation = animation;
-	}
-
 	public abstract void render(float f5);
 }
