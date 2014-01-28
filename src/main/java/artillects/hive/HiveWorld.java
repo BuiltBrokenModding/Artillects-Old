6d20fae3b50ff48929d42a778651502915d18a2b
DarkGuardsman <rseifert.phone@gmail.com>
2014-01-28 15:15:16 -0500

    Started on per world hive mind

---
+++ src/main/java/artillects/hive/HiveWorld.java
@@ -0,0 +1,29 @@
+package artillects.hive;
+
+import net.minecraft.world.World;
+
+/** Hive worlds are instances of the hive mind that only function with in one world.
+ * 
+ * @author Darkguardsman */
+public class HiveWorld
+{
+    private World world = null;
+
+    public HiveWorld(World world)
+    {
+        this.world = world;
+    }
+
+    public World getWorld()
+    {
+        return this.world;
+    }
+    
+    public void update()
+    {
+        synchronized(world)
+        {
+            
+        }
+    }
+}
