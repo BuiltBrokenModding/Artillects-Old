61a4f200d418f4c0caa7ae207dcab1d13850f133
Robert S <rseifert.phone@gmail.com>
2014-07-05 02:31:22 -0400

    Added to plane connection unit test

--- src/test/java/test/common/PlaneTest.java
+++ src/test/java/test/common/PlaneTest.java
@@ -16,15 +16,29 @@ public class PlaneTest
         System.out.println("Start: " + start);
         System.out.println("End: " + end);
         System.out.println("Plane: " + plane);
+        
+        
         System.out.println("\n==Connection Test==");
 
         Plane centerPlane = new Plane(-1, -1, 1, 1);
-        Plane leftPlane = new Plane(-2, -2, -1, -1);
+        Plane leftPlane = new Plane(-2, -1, -1, 1);
+        Plane rightPlane = new Plane(1, -1, 3, 1);
+        Plane topPlane = new Plane(-1, 1, 1, 3);
+        Plane bottomPlane = new Plane(-1, -3, 1, -1);
+        
+        Plane plane1 = new Plane(10, 10, 12, 12);
         
         System.out.println("CenterPlane: " + centerPlane);
         System.out.println("LeftPlane: " + leftPlane);
+        System.out.println("RightPlane: " + rightPlane);
+        System.out.println("TopPlane: " + topPlane);
+        System.out.println("BottomPlane: " + bottomPlane);
         
         System.out.println("LeftToCenter: " + centerPlane.isConnected(leftPlane));
+        System.out.println("RightToCenter: " + centerPlane.isConnected(rightPlane));
+        System.out.println("TopToCenter: " + centerPlane.isConnected(topPlane));
+        System.out.println("BottomToCenter: " + centerPlane.isConnected(bottomPlane));
+        System.out.println("r1ToCenter: " + centerPlane.isConnected(plane1));
 
     }
 }
