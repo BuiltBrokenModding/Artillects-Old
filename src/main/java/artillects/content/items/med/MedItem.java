07aec5cd41baedfc898ebe42a581fdc8cb64a60b
Robert S <rseifert.phone@gmail.com>
2014-10-04 01:02:36 -0400

    Added 6 bandage package

--- src/main/java/artillects/content/items/med/MedItem.java
+++ src/main/java/artillects/content/items/med/MedItem.java
@@ -10,18 +10,28 @@ import net.minecraft.item.ItemStack;
  */
 public enum MedItem
 {
-    BANDAGE(2, new ItemStack(Artillects.itemBandage, 1, 1)),
-    USED_BANDAGE;
+    //Single bandage
+    /* 0 */BANDAGE(2, new ItemStack(Artillects.itemBandage, 1, 1)),
+    /* 1 */USED_BANDAGE,
+
+    //Package of bandages
+    /* 2 */BANDAGE_PACK_SIX(2, new ItemStack(Artillects.itemBandage, 1, 1), new ItemStack(Artillects.itemBandage, 3, 1)),
+    /* 3 */BANDAGE_PACK_FIVE(2, new ItemStack(Artillects.itemBandage, 1, 1), new ItemStack(Artillects.itemBandage, 4, 1)),
+    /* 4 */BANDAGE_PACK_FOUR(2, new ItemStack(Artillects.itemBandage, 1, 1), new ItemStack(Artillects.itemBandage, 5, 1)),
+    /* 5 */BANDAGE_PACK_THREE(2, new ItemStack(Artillects.itemBandage, 1, 1), new ItemStack(Artillects.itemBandage, 6, 1)),
+    /* 6 */BANDAGE_PACK_TWO(2, new ItemStack(Artillects.itemBandage, 1, 1), new ItemStack(Artillects.itemBandage, 7, 1)),
+    /* 7 */BANDAGE_PACK_ONE(2, new ItemStack(Artillects.itemBandage, 1, 1), new ItemStack(Artillects.itemBandage, 8, 1)),
+    /* 8 */BANDAGE_PACK_ZERO;
 
     float healBy = 2;
-    ItemStack returnStack = null;
+    ItemStack[] returnStack = null;
 
     private MedItem()
     {
         this(0, null);
     }
 
-    private MedItem(float healBy, ItemStack used)
+    private MedItem(float healBy, ItemStack... used)
     {
         this.healBy = healBy;
         returnStack = used;
