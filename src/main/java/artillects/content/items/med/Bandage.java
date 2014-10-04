bc9a6f0820f775953deaf80634a1f9431b97b7b8
HangCow <hangcow@yahoo.com>
2014-10-03 23:06:36 -0400

    Created Bandage item

---
+++ src/main/java/artillects/content/items/med/Bandage.java
@@ -0,0 +1,15 @@
+package artillects.content.items.med;
+
+import artillects.core.Reference;
+import net.minecraft.item.Item;
+
+/**
+ * Created by COWPIE on 10/3/2014.
+ */
+public class Bandage extends Item
+{
+    public Bandage()
+    {
+        this.setUnlocalizedName(Reference.PREFIX + "bandage");
+    }
+}
