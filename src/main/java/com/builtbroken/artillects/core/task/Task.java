01777c2f914829469962897fd2405f23074892a5
Robert Seifert <rseifert.phone@gmail.com>
2016-03-25 08:19:00 -0400

    Added task objects as a reminder to implement later

---
+++ src/main/java/com/builtbroken/artillects/core/task/Task.java
@@ -0,0 +1,14 @@
+package com.builtbroken.artillects.core.task;
+
+/**
+ * Objective for an NPC to complete
+ *
+ * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
+ * Created by Dark(DarkGuardsman, Robert) on 3/25/2016.
+ */
+public class Task
+{
+    boolean repeat = false;
+
+    String name;
+}
---
+++ src/main/java/com/builtbroken/artillects/core/task/military/Guard.java
@@ -0,0 +1,11 @@
+package com.builtbroken.artillects.core.task.military;
+
+import com.builtbroken.artillects.core.task.Task;
+
+/**
+ * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
+ * Created by Dark(DarkGuardsman, Robert) on 3/25/2016.
+ */
+public class Guard extends Task
+{
+}
